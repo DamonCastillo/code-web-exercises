7842d38f886aa03cb0ae79b51fc5aa17f81d3efa
Damon Castillo <damono7.castillo@gmail.com>
2022-04-22 09:45:17 -0500

    Warmup.js

--- js/warmups.js
+++ js/warmups.js
@@ -1,9 +1,35 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>External JS</title>
-</head>
-<body>
-<script src="js/external.js"></script>
-</body>
-</html>
+"use strict";
+
+let max = "..........".length;
+
+for (let i = ".".length; i <= (max * max); i++) {
+    if (i % 15 === 0 ) {
+        console.log(i + "  is FizzBuzz");
+    } else if (i % 5 === 0) {
+        console.log(i + " is Buzz");
+    }else if (i % 3 === 0 ) {
+        console.log(i + " is Fizz")
+    }else {
+    console.log(i);
+    }
+}
+
+// Instructor Code
+
+// function fizzBuzz(limit) {
+//     if (limit === undefined) {
+//         limit = 100;
+//     }
+//     for (let i = 1; i <= limit; i++) {
+//         if (i % 15 === 0) {
+//             console.log("Fizz");
+//         } else if (i % 5 === 0) {
+//             console.log("Buzz");
+//         } else if (i % 3 === 0) {
+//             console.log("FizzBuzz");
+//         } else {
+//             console.log(i)
+//         }
+//     }
+// }
+// fizzBuzz(i);
