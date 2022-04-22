e4e297fae13f314a283452a4e4a2a4ee1c94bfb6
Damon Castillo <damono7.castillo@gmail.com>
2022-04-22 11:28:58 -0500

    while loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,42 +1,43 @@
 "use strict";
+(() => {
 
-
-const randomBetween = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
+    const randomBetween = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
 
 // 2
-function showMutiplicationTable(num) {
-    for (let i = 1; i < 11; i++) {
-        let answer = i * num;
-        let str = num + ' x ' + i + ' = ' + answer
-        console.log(str);
+    function showMutiplicationTable(num) {
+        for (let i = 1; i < 11; i++) {
+            let answer = i * num;
+            let str = num + ' x ' + i + ' = ' + answer
+            console.log(str);
+        }
     }
-}
 
-showMutiplicationTable(7)
+    showMutiplicationTable(7)
 
 // 3
-for (let i = 0; i < 10; i++) {
-    let random = randomBetween(20, 200);
-    if (random % 2 === 0) {
-        console.log(random + " is Even.");
-    } else {
-        console.log(random + " is Odd");
+    for (let i = 0; i < 10; i++) {
+        let random = randomBetween(20, 200);
+        if (random % 2 === 0) {
+            console.log(random + " is Even.");
+        } else {
+            console.log(random + " is Odd.");
+        }
     }
-}
 
 // 4
-let str = '';
+    let str = '';
 
-for (let i = 0; i <= 9; i++) {
-    for (let j = 1; j <= i; j++) {
-        str += i;
+    for (let i = 0; i <= 9; i++) {
+        for (let j = 1; j <= i; j++) {
+            str += i;
+        }
+        str += '\n';
     }
-    str += '\n';
-}
-console.log(str);
+    console.log(str);
 
 // 5
-    for (let i = 100; i > 0; i-=5 ){
-  console.log(i);
+    for (let i = 100; i > 0; i -= 5) {
+        console.log(i);
     }
 
+})();
--- js/while.js
+++ js/while.js
@@ -1,9 +1,29 @@
-"use strict"
+"use strict";
+
 // while loop
 
+function multipleByTwo() {
+    let x = 1
+    while (x < 65536) {
+        x *= 2;
+        console.log(x);
+    }
+}
+
+multipleByTwo()
 
 
-
-
-
-
+const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
+let inventory = randomNumber(50, 100);
+let customerOrder = randomNumber(1, 5);
+do {
+    console.log("customer orders " + customerOrder + " cone(s). We have " + inventory + " left in stock");
+} if (customerOrder > inventory) {
+    console.log(" My apologies, we can't fulfill your order. ");
+} else if {
+(customerOrder -= inventory);
+    console.log("Here you go!, We have " + inventory + " left in stock");
+}
+while (inventory > 0) {
+    console.log(" Daily mission complete, all cones sold");
+}
