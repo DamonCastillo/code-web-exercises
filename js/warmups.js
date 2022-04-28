6c00f09ff72eb37efc9cc507f19683e64395bb5a
Damon Castillo <damono7.castillo@gmail.com>
2022-04-28 09:15:39 -0500

    warm-ups

--- js/warmups.js
+++ js/warmups.js
@@ -1,18 +1,18 @@
 "use strict";
 
-let max = "..........".length;
-
-for (let i = ".".length; i <= (max * max); i++) {
-    if (i % 15 === 0 ) {
-        console.log(i + "  is FizzBuzz");
-    } else if (i % 5 === 0) {
-        console.log(i + " is Buzz");
-    }else if (i % 3 === 0 ) {
-        console.log(i + " is Fizz")
-    }else {
-    console.log(i);
-    }
-}
+// let max = "..........".length;
+//
+// for (let i = ".".length; i <= (max * max); i++) {
+//     if (i % 15 === 0 ) {
+//         console.log(i + "  is FizzBuzz");
+//     } else if (i % 5 === 0) {
+//         console.log(i + " is Buzz");
+//     }else if (i % 3 === 0 ) {
+//         console.log(i + " is Fizz")
+//     }else {
+//     console.log(i);
+//     }
+// }
 
 // Instructor Code
 
@@ -35,15 +35,28 @@ for (let i = ".".length; i <= (max * max); i++) {
 // fizzBuzz(i);
 
 //April 27th  Warm-ups
-    const animals =["dog", "cat", "Frog", "Bat"]
+//     const animals =["dog", "cat", "Frog", "Bat"]
+//
+//     function firstLetter(animals){
+//         let bucket = "";
+//     for (let el in animals) {
+//         bucket = +el[0]
+//
+//
+//     }
+// }
+// firstLetter()
 
-    function firstLetter(animals){
-        let bucket = "";
-    for (let el in animals) {
-        bucket = +el[0]
+// April 28th Warm-ups
+
+function alphabetOrder(str){
+   // let strArray = str.split('');
+   // console.log(strArray)
+   //  let sortedArray = strArray.sort()
+   //  console.log(sortedArray);
+   // return sortedArray
+    return str.split('').sort().join('')
 
 
-    }
 }
-firstLetter()
-
+console.log(alphabetOrder("codeup"))
