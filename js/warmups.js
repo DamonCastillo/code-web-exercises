13b3823c4a5a1da827808e7ddd7a998b55afe509
Damon Castillo <damono7.castillo@gmail.com>
2022-05-09 09:32:04 -0500

    updated warm-ups

--- js/warmups.js
+++ js/warmups.js
@@ -104,3 +104,47 @@
 // console.log(returnProductEssentialDetails(product3));;
 
 //Git review
+
+const hamsters = [
+    {
+        name: "Hamtaro",
+        heightInMM: 86,
+        fur: ['orange', 'white'],
+        gender: "male",
+        dateOfBirth: "August 6"
+    } , {
+        name: "Bijou",
+        heightInMM: 75,
+        fur: ['white'],
+        gender: "female",
+        dateOfBirth: "July 10"
+    } , {
+        name: "Oxnard",
+        heightInMM: 100,
+        fur: ['grey', 'white'],
+        gender: "male",
+        dateOfBirth: "May 3"
+    } , {
+        name: "Boss",
+        heightInMM: 120,
+        fur: ['brown', 'white'],
+        gender: "male",
+        dateOfBirth: "Spetember 21"
+    } , {
+        name: "Snoozer",
+        heightInMM: 85,
+        fur: ['brown', 'white', "pink"],
+        gender: "male",
+        dateOfBirth: "January 14"
+    }
+];
+
+function getTallest(arr) {
+    let tallest = {heightInMM: 0};
+    for (let hamster of arr) {
+        if (hamster.heightInMMv > tallest.heightInMM) {
+            tallest = hamster
+        }
+    }
+}
+console.log(getTallest(hamsters));
