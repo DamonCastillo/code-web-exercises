7de8137740ae1b6d8e04721854453d7a3012f530
Damon Castillo <damono7.castillo@gmail.com>
2022-05-31 15:54:21 -0500

    flexbox update

--- js/weather_map.js
+++ js/weather_map.js
@@ -3,8 +3,11 @@ const URL = "https://api.openweathermap.org/data/2.5/onecall";
 
 $(function ()
 {
-    $('img').mouseenter(function () {
-        $('#map').css('background', 'black')
+    $('.flip-card').mouseenter(function () {
+        $('#blur').css("display","block")
+    })
+    $('.flip-card').mouseleave(function () {
+        $('#blur').css("display","none")
     })
 
 
@@ -14,24 +17,24 @@ $(function ()
 
 
 
-// $.get(URL, {
-//     APPID: OPEN_WEATHER_KEY,
-//     lat: 40.74,
-//     lon: -73.98,
-//     unit: "imperial"
-// }).done(function (results) {
-//     console.log(results)
-// })
-//
-// geocode("San Antonio, US", MAP_API_KEY).then(function (data) {
-//     console.log(data)
-//     $.get(URL, {
-//         APPID: OPEN_WEATHER_KEY,
-//         lat: data[1],
-//         lon: data[0],
-//         unit: "imperial"
-//     }).done(function (results) {
-//         console.log(results)
-//     })
-// });
+$.get(URL, {
+    APPID: OPEN_WEATHER_KEY,
+    lat: 40.74,
+    lon: -73.98,
+    unit: "imperial"
+}).done(function (results) {
+    console.log(results)
+})
+
+geocode("San Antonio, US", MAP_API_KEY).then(function (data) {
+    console.log(data)
+    $.get(URL, {
+        APPID: OPEN_WEATHER_KEY,
+        lat: data[1],
+        lon: data[0],
+        unit: "imperial"
+    }).done(function (results) {
+        console.log(results)
+    })
+});
 
